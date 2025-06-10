5376c690c696673346f8f03a092a635223bbc945
luckycyang <kfove.liang@protonmail.com>
2025-06-11 01:41:42 +0800

    maybeok

--- zfc/CompileCommand.zig
+++ zfc/CompileCommand.zig
@@ -7,7 +7,7 @@ file: []const u8,
 
 pub fn fromLinkObject(o: *LinkObject) CompileCommand {
     const ans: CompileCommand = .{ .directory = &.{}, .arguments = &.{}, .file = &.{} };
-    switch (o) {
+    switch (o.*) {
         .c_source_file => |i| std.debug.print("{any}\r\n", .{i}),
         .c_source_files => |i| std.debug.print("{any}\r\n", .{i}),
         _ => {},
